57ea67557241cfca5a54c18ec5edc9b6eb7973c5
MuhammedSm <muhammedseyhmusa5@gmail.com>
2023-07-07 20:52:30 +0300

    edit functionality

--- components/Todo.js
+++ components/Todo.js
@@ -1,20 +1,51 @@
 import { TableCell, TableContainer, Table, TableBody, TableRow, Paper, IconButton } from '@mui/material';
 import moment from 'moment/moment';
-import React from 'react';
+import React, { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 import { db } from '@/firebase';
-import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
+import CheckIcon from '@mui/icons-material/Check';
+import CancelIcon from '@mui/icons-material/Cancel';
+
 
 const Todo = ({ id, timestamp, title, detail }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+  const [editedDetail, setEditedDetail] = useState(detail);
+
   const handleDelete = async () => {
     try {
       const todoRef = doc(db, `todos/${id}`);
-      console.log('Deleting todo with ID:', id);
       await deleteDoc(todoRef);
       console.log('Todo deleted successfully.');
     } catch (error) {
       console.error('Error deleting todo:', error);
     }
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    try {
+      const todoRef = doc(db, `todos/${id}`);
+      await updateDoc(todoRef, {
+        title: editedTitle,
+        detail: editedDetail
+      });
+      console.log('Todo updated successfully.');
+      setIsEditing(false);
+    } catch (error) {
+      console.error('Error updating todo:', error);
+    }
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setEditedTitle(title);
+    setEditedDetail(detail);
   };
 
   return (
@@ -23,7 +54,15 @@ const Todo = ({ id, timestamp, title, detail }) => {
         <TableBody>
           <TableRow>
             <TableCell sx={{ paddingTop: '10px', paddingBottom: '10px' }}>
-              <strong>Title:</strong> {title}
+              <strong>Title:</strong> {isEditing ? (
+                <input
+                  type="text"
+                  value={editedTitle}
+                  onChange={(e) => setEditedTitle(e.target.value)}
+                />
+              ) : (
+                title
+              )}
             </TableCell>
           </TableRow>
           <TableRow>
@@ -33,14 +72,37 @@ const Todo = ({ id, timestamp, title, detail }) => {
           </TableRow>
           <TableRow>
             <TableCell sx={{ paddingTop: '10px', paddingBottom: '10px' }}>
-              <strong>Detail:</strong> {detail}
+              <strong>Detail:</strong> {isEditing ? (
+                <textarea
+                  value={editedDetail}
+                  onChange={(e) => setEditedDetail(e.target.value)}
+                />
+              ) : (
+                detail
+              )}
             </TableCell>
           </TableRow>
           <TableRow>
             <TableCell align="right" sx={{ paddingTop: '10px', paddingBottom: '10px' }}>
-              <IconButton onClick={handleDelete}>
-                <DeleteIcon />
-              </IconButton>
+              {isEditing ? (
+                <>
+                  <IconButton onClick={handleSave}>
+                    <CheckIcon />
+                  </IconButton>
+                  <IconButton onClick={handleCancel}>
+                    <CancelIcon />
+                  </IconButton>
+                </>
+              ) : (
+                <>
+                  <IconButton onClick={handleEdit}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={handleDelete}>
+                    <DeleteIcon />
+                  </IconButton>
+                </>
+              )}
             </TableCell>
           </TableRow>
         </TableBody>
